35d6bf09cdf591f8720fa410f5c25d3c973d1bb5
JorgeSDiaz <saenzdiazj0@gmail.com>
2024-03-28 15:05:26 -0500

    fix(app): :heavy_minus_sign: remove unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from './domains/shared/components/navbar/navbar.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet],
   template: '<router-outlet />',
 })
 export class AppComponent {
