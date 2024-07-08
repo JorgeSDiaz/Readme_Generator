1ae8cf8c0b0f1464c73b64c612f129de460bb919
JorgeSDiaz <saenzdiazj0@gmail.com>
2024-07-08 21:37:01 +0200

    style(Project): :lipstick: Add app basic structure

--- src/app/pages/readme-generator/readme-generator.component.ts
+++ src/app/pages/readme-generator/readme-generator.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { EditorComponent } from "../../shared/components/editor/editor.component";
-import { PreviewComponent } from "../../shared/components/preview/preview.component";
+import { EditorComponent } from '../../shared/components/editor/editor.component';
+import { PreviewComponent } from '../../shared/components/preview/preview.component';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { ButtonGroupModule } from 'primeng/buttongroup';
 
 @Component({
-    selector: 'app-readme-generator',
-    standalone: true,
-    templateUrl: './readme-generator.component.html',
-    styleUrl: './readme-generator.component.css',
-    imports: [EditorComponent, PreviewComponent]
+  selector: 'app-readme-generator',
+  standalone: true,
+  templateUrl: './readme-generator.component.html',
+  styleUrl: './readme-generator.component.css',
+  imports: [
+    EditorComponent,
+    PreviewComponent,
+    SidebarModule,
+    ButtonModule,
+    DividerModule,
+    ButtonGroupModule
+  ],
 })
 export class ReadmeGeneratorComponent {
-
+  sidebarVisible: boolean = false;
 }
